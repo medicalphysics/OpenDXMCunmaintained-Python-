3cad7bc7f482d9badbf63cd29f29614c22a2d762
Erlend Andersen <andersen.erlend@gmail.com>
2017-09-11 15:12:31 +0200

    Added option for dual source

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['scipy', 'numpy', 'tables', 'pydicom', 'pyqtgraph', 'pyopengl'],
+    install_requires=['scipy', 'numpy', 'tables', 'pydicom', 'pyqtgraph', 'pyopengl', 'PyQt4'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
